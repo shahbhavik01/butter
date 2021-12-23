f4fe3503b67dceb22dd9ffbd8d97b03cbab2bde3
Bhavik Shah <shahbhavik002@gmail.com>
2021-12-23 08:47:15 -0800

    GraphQL

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "butterimages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
